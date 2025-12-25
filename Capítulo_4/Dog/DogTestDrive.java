3c65cbf11695e8fe0c2f73510075fdc046df6bed
DenisPython1987 <denisandervivan2@gmail.com>
2025-12-25 09:56:50 -0300

    Exercício do livro

---
+++ Capítulo_4/Dog/DogTestDrive.java
@@ -0,0 +1,18 @@
+package Capítulo_4.Dog;
+
+public class DogTestDrive {
+    public static void main(String[] args){
+        Dog one = new Dog();
+        one.size = 70;
+
+        Dog two = new Dog();
+        two.size = 8;
+
+        Dog three = new Dog();
+        three.size = 35;
+
+        one.bark(3);
+        //two.bark();
+        //three.bark();
+    }
+}
