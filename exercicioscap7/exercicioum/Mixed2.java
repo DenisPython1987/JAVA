7a6cb0f9aa82ca449b3f419b9bcf7dce6bd01e5a
DenisPython1987 <denisandervivan2@gmail.com>
2026-01-12 21:59:54 -0300

    Exercícios do capítulo 7

---
+++ exercicioscap7/exercicioum/Mixed2.java
@@ -0,0 +1,12 @@
+package exercicioscap7.exercicioum;
+
+public class Mixed2 {
+    public static void main(String[] args){
+        A a = new A();
+        B b = new B();
+        C c = new C();
+        A a2 = new C();
+
+        
+    }
+}
